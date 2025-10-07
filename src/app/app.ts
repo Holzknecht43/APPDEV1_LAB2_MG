939a1c97abfbbb538fdc36fd9cdb9f292ca49086
Marc Holzknecht <holzknecht_m@ca01.ub.lan>
2025-10-07 12:38:50 +0800

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
+import { TaskService } from './task-service'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,  RouterModule, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
